7edb8b1e8070736d75308521ff21a2d511913d82
NGOMA UMBA Tamalie <128788295+tamalie-ngomaUmba-vinci@users.noreply.github.com>
2025-08-28 15:04:43 +0200

    ajout documents pour d'éventuels test

---
+++ spec/support/appTest.js
@@ -0,0 +1,8 @@
+import {Main} from '../src/app.js';
+
+const main = new Main();
+
+describe('App Test', () => {
+    it("ce qui est attendu", function() {
+        expect(main.fonction('parametre')).toBe('parametre');
+    })});
---
+++ src/app.js
@@ -0,0 +1,8 @@
+export class Main {
+    fonction(param) {
+        // faire des actions
+
+        return param; // retour du param modifié
+
+    }
+}
